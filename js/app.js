b19196ff5e8a7fdc60ab03a78a9546e6a496da30
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2015-10-09 21:04:59 +0200

    Aggiunto salvataggio su localstorag/cookies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,50 @@
+var dataStore = (function() {
+    var set, get;
+    var storageAvailable = function(type) {
+        // Check if localStorage is available
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    };
+
+    if (storageAvailable('localStorage')) {
+        // We can use localStorage
+        set = function(k, v) {
+            return localStorage.setItem(k, JSON.stringify(v));
+        };
+        get = function(k) {
+            var data = localStorage.getItem(k);
+            return (!data) ? [] : JSON.parse(data);
+        };
+    } else {
+        // Too bad, no localStorage for us
+        // Use cookies instead
+        set = function(k, v) {
+            return $cookies.put(k, JSON.stringify(v));
+        };
+        get = function(k) {
+            var cookie = $cookies.get(k);
+            return (!cookie) ? [] : JSON.parse(cookie);
+        };
+    }
+
+	return {
+		set: set,
+		get: get
+	};
+
+})();
+
 var app = (function() {
-    var tasks = [];
+    var saveTasks = function(list) {return dataStore.set('jqftasks', list);};
+    var loadTasks = function() {return dataStore.get('jqftasks');};
+    var tasks = loadTasks();
 
     var checkInput = function(item) {
         if (!item.name) return false;
@@ -7,8 +52,6 @@ var app = (function() {
         //TODO notify
     };
 
-	var saveTasks = function () {
-	};
 
     var addTask = function(name, desc) {
         var item = {
@@ -17,35 +60,41 @@ var app = (function() {
         };
         if (checkInput(item)) {
             tasks.push(item);
+			saveTasks(tasks);
         } else {
             //TODO notify
         }
     };
 
     var display = function(elementID) {
-		$(elementID).empty();
-		$.each(tasks, function (index, value) {
-			var listItem = $('<li>', {
-				'id' : 'task' + index,
-				'class': 'task-item'
-			});
+        $(elementID).empty();
+        $.each(tasks, function(index, value) {
+            var listItem = $('<li>', {
+                'id': 'task' + index,
+                'class': 'task-item'
+            });
 
-			$('<h3>', {'text': value.name}).appendTo(listItem);
-			$('<p>', {'text': value.desc}).appendTo(listItem);
+            $('<h3>', {
+                'text': value.name
+            }).appendTo(listItem);
+            $('<p>', {
+                'text': value.desc
+            }).appendTo(listItem);
 
 
-			$(listItem).appendTo(elementID);
-		});
-	};
+            $(listItem).appendTo(elementID);
+        });
+    };
 
-	return {
-		addTask: addTask,
-		display: display
-	};
+    return {
+        addTask: addTask,
+        display: display
+    };
 
 })();
 
 $(document).ready(function() {
+	app.display('#display-tasks');
 
     // Event listener per il tastone blu
     $('#new-task-submit').click(function(e) {
@@ -54,7 +103,7 @@ $(document).ready(function() {
             $('#new-task-title').val(),
             $('#new-task-description').val()
         );
-		app.display('#display-tasks');
+        app.display('#display-tasks');
     });
 
 });
